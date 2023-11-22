ddff62320ff00acbd123c7cc9a5a1bb3c6164d8c
pietrostraneo <straneo_p@yahoo.it>
2023-11-22 16:29:57 +0100

    ESERCIZIO COMPLETATO

--- js/script.js
+++ js/script.js
@@ -1,17 +1,23 @@
 let i;
 let f = "FIZZ"
 let b = "BUZZ"
+const list = document.querySelector("ul");
+let li;
 
 for(i=1; i<=100; i++){
     console.log(i)
-
+    li = `<li>${i}</li>`
+    list.innerHTML += li
     if(i%15 == 0){
         console.log(i + " " + f + " " + b)
+        list.innerhtml += f + b
     }
     else if(i%3 == 0){
         console.log(i + " " + f)
+        list.innerHTML += f
     }
     else if(i%5 == 0){
         console.log(i + " " + b)
+        list.innerHTML += b
     }
 }
